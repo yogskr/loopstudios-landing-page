8420601c86835d4137d44e60d016d37526fdc81d
yogskr <yogakrisanta@gmail.com>
2024-03-07 22:50:18 +0700

    Deleted unused className properties

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -6,7 +6,6 @@ export default function NavMenu({ isHeader }) {
   return navTexts.map((text) => (
     <li
       className="group hover:cursor-pointer hover:border-b-2 hover:border-white hover:pb-3 hover:transition-all"
-      // className="group relative hover:cursor-pointer hover:border-white hover:pb-3 hover:before:absolute hover:before:bottom-0 hover:before:left-auto hover:before:right-[1.7rem] hover:before:h-2 hover:before:w-1/2 hover:before:border-b-2 hover:before:content-['']"
       key={text.id}
     >
       {isHeader
