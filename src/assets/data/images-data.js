b4bfc694a8239bcce2138437f1f1f84c48c145ac
yogskr <yogakrisanta@gmail.com>
2024-03-07 16:53:19 +0700

    Run the "npm run build" for deployment

--- src/assets/data/images-data.js
+++ src/assets/data/images-data.js
@@ -1,32 +1,32 @@
 export const logo = {
-  src: "./src/assets/images/logo.svg",
+  src: "/images/logo.svg",
   alt: "Loopstudios logo",
 };
 
 export const about = {
-  srcMobile: "./src/assets/images/mobile/image-interactive.jpg",
-  srcDesktop: "./src/assets/images/desktop/image-interactive.jpg",
+  srcMobile: "/images/mobile/image-interactive.jpg",
+  srcDesktop: "/images/desktop/image-interactive.jpg",
   alt: "A man playing with a VR device",
 };
 
 export const social = [
   {
     id: 0,
-    src: "./src/assets/images/icon-facebook.svg",
+    src: "/images/icon-facebook.svg",
     alt: "Facebook logo",
   },
   {
     id: 1,
-    src: "./src/assets/images/icon-twitter.svg",
+    src: "/images/icon-twitter.svg",
     alt: "Instagram logo",
   },
   {
     id: 2,
-    src: "./src/assets/images/icon-pinterest.svg",
+    src: "/images/icon-pinterest.svg",
     alt: "Pinterest logo",
   },
   {
     id: 3,
-    src: "./src/assets/images/icon-instagram.svg",
+    src: "/images/icon-instagram.svg",
   },
 ];
