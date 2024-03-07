8b13bc1ef85219603031538eacdb05d79f8e713c
yogskr <yogakrisanta@gmail.com>
2024-03-07 22:50:38 +0700

    Changed <li> to <figure>

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -2,11 +2,11 @@ import { social } from "../assets/data/images-data";
 
 export default function SocialMedia() {
   return social.map((image) => (
-    <li
+    <figure
       key={image.id}
       className="group flex h-10 items-center hover:cursor-pointer hover:border-b-2 hover:border-white hover:pb-3 hover:transition-all"
     >
       <img src={image.src} alt={image.alt} />
-    </li>
+    </figure>
   ));
 }
