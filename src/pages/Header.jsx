cb0ad4eddd4c09a9a93c3684c2e5adad70f9295a
yogskr <yogakrisanta@gmail.com>
2024-03-07 22:51:43 +0700

    Changed font size to 4rem when min-width is 1440px

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="flex h-screen w-fit flex-col bg-heroPatternMobile bg-cover lg:h-[650px] lg:w-full lg:bg-heroPatternDesktop">
       <NavBar />
       <div className="flex h-full flex-col items-center justify-center bg-black/45 px-5 md:px-10 lg:items-start lg:px-20 xl:px-40 xl:pt-16 2xl:px-52">
-        <h1 className="h-fit border-[3px] p-5 font-josefinSans text-[2.7rem] font-[300] leading-none text-white md:text-7xl lg:w-[58%] lg:p-10 lg:text-5xl xl:text-6xl 2xl:w-[50%] 2xl:text-7xl">
+        <h1 className="h-fit border-[3px] p-5 font-josefinSans text-[2.7rem] font-[300] leading-none text-white md:text-7xl lg:w-[58%] lg:p-10 lg:text-5xl xl:text-[4rem] 2xl:w-[50%] 2xl:text-7xl">
           {uppercaseLetter(headerText.title)}
         </h1>
       </div>
