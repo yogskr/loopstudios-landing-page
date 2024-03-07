ba66963214de0a4ff2876bfd4b744d6f4fd97298
yogskr <yogakrisanta@gmail.com>
2024-03-07 16:52:07 +0700

    Modified the file path

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,37 +9,26 @@ export default {
       },
       backgroundImage: {
         // Hero Pattern
-        heroPatternMobile: "url('./src/assets/images/mobile/image-hero.jpg')",
-        heroPatternDesktop: "url('./src/assets/images/desktop/image-hero.jpg')",
+        heroPatternMobile: "url('/images/mobile/image-hero.jpg')",
+        heroPatternDesktop: "url('/images/desktop/image-hero.jpg')",
         // Project Background
-        deepEarthMobile:
-          "url('./src/assets/images/mobile/image-deep-earth.jpg')",
-        deepEarthDesktop:
-          "url('./src/assets/images/desktop/image-deep-earth.jpg')",
-        nightArcadeMobile:
-          "url('./src/assets/images/mobile/image-night-arcade.jpg')",
-        nightArcadeDesktop:
-          "url('./src/assets/images/desktop/image-night-arcade.jpg')",
-        soccerTeamMobile:
-          "url('./src/assets/images/mobile/image-soccer-team.jpg')",
-        soccerTeamDesktop:
-          "url('./src/assets/images/desktop/image-soccer-team.jpg')",
-        theGridMobile: "url('./src/assets/images/mobile/image-grid.jpg')",
-        theGridDesktop: "url('./src/assets/images/desktop/image-grid.jpg')",
-        fromAboveMobile:
-          "url('./src/assets/images/mobile/image-from-above.jpg')",
-        fromAboveDesktop:
-          "url('./src/assets/images/desktop/image-from-above.jpg')",
-        pocketBorealisMobile:
-          "url('./src/assets/images/mobile/image-pocket-borealis.jpg')",
+        deepEarthMobile: "url('/images/mobile/image-deep-earth.jpg')",
+        deepEarthDesktop: "url('/images/desktop/image-deep-earth.jpg')",
+        nightArcadeMobile: "url('/images/mobile/image-night-arcade.jpg')",
+        nightArcadeDesktop: "url('/images/desktop/image-night-arcade.jpg')",
+        soccerTeamMobile: "url('/images/mobile/image-soccer-team.jpg')",
+        soccerTeamDesktop: "url('/images/desktop/image-soccer-team.jpg')",
+        theGridMobile: "url('/images/mobile/image-grid.jpg')",
+        theGridDesktop: "url('/images/desktop/image-grid.jpg')",
+        fromAboveMobile: "url('/images/mobile/image-from-above.jpg')",
+        fromAboveDesktop: "url('/images/desktop/image-from-above.jpg')",
+        pocketBorealisMobile: "url('/images/mobile/image-pocket-borealis.jpg')",
         pocketBorealisDesktop:
-          "url('./src/assets/images/desktop/image-pocket-borealis.jpg')",
-        theCuriosityMobile:
-          "url('./src/assets/images/mobile/image-curiosity.jpg')",
-        theCuriosityDesktop:
-          "url('./src/assets/images/desktop/image-curiosity.jpg')",
-        fishEyeMobile: "url('./src/assets/images/mobile/image-fisheye.jpg')",
-        fishEyeDesktop: "url('./src/assets/images/desktop/image-fisheye.jpg')",
+          "url('/images/desktop/image-pocket-borealis.jpg')",
+        theCuriosityMobile: "url('/images/mobile/image-curiosity.jpg')",
+        theCuriosityDesktop: "url('/images/desktop/image-curiosity.jpg')",
+        fishEyeMobile: "url('/images/mobile/image-fisheye.jpg')",
+        fishEyeDesktop: "url('/images/desktop/image-fisheye.jpg')",
       },
       textColor: {
         darkGray: "hsl(0, 0%, 55%)",
