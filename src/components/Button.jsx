5363d75958326b81173de5f60e2ecbfa97719121
yogskr <yogakrisanta@gmail.com>
2024-03-07 22:48:03 +0700

    Added transition duration to 300ms

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ export default function Button() {
   return (
     <>
       <button
-        className={`border border-black bg-white p-[0.6rem] px-12 align-middle font-josefinSans font-semibold tracking-widest hover:cursor-pointer hover:bg-black hover:text-white hover:transition md:text-[1.1rem]`}
+        className={`border border-black bg-white p-[0.6rem] px-12 align-middle font-josefinSans font-semibold tracking-widest hover:cursor-pointer hover:bg-black hover:text-white hover:transition-all hover:duration-300 md:text-[1.1rem]`}
       >
         {uppercaseLetter(projectText.button)}
       </button>
